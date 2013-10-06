eb3221f48e44aee12836a91030d9b5e6483e4d8e
Kyle Anderson <kyle@xkyle.com>
2013-10-06 12:08:35 -0700

    Added context to btsync spec

--- spec/classes/btsync_spec.rb
+++ spec/classes/btsync_spec.rb
@@ -1,20 +1,22 @@
 require 'spec_helper'
 
 describe 'btsync', :type => :class do
-  let :facts do
-    {
-      :osfamily               => 'Debian',
-      :concat_basedir         => '/dne',
+  context "On Debian OSes" do
+    let :facts do
+      {
+        :osfamily               => 'Debian',
+        :concat_basedir         => '/dne',
+      }
+    end
+    it { should contain_service("btsync").with(
+      'ensure'    => 'running',
+      'enable'    => 'true',
+      )
+    }
+    it { should contain_file("/usr/bin/btsync").with(
+      'ensure'  => 'present',
+      'notify'  => 'Service[btsync]',
+      )
     }
   end
-  it { should contain_service("btsync").with(
-    'ensure'    => 'running',
-    'enable'    => 'true',
-    )
-  }
-  it { should contain_file("/usr/bin/btsync").with(
-    'ensure'  => 'present',
-    'notify'  => 'Service[btsync]',
-    )
-  }
 end
